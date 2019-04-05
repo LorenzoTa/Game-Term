424501b2bc876fa23027f02d1599984afd96d9be
LorenzoTa <LorenzoTa@users.noreply.github.com>
2019-04-05 17:45:48 +0200

    ??

---
+++ lib/Game/Term/Game.pm
@@ -0,0 +1,57 @@
+package Game::Term::Game;
+
+use 5.014;
+use strict;
+use warnings;
+
+use Game::Term::Configuration;
+use Game::Term::UI;
+
+our $VERSION = '0.01';
+
+sub new{
+	my $class = shift;
+	my %param = @_;
+	# GET hero..
+	# if $param{hero} or ..	
+	$param{configuration} //= Game::Term::Configuration->new();
+	
+	$param{scenario} //= Game::Term::Scenario->new( );
+	
+	$param{ui} //= Game::Term::UI->new( 
+										configuration => $param{configuration}, 
+										# map => $param{map},
+										map => $param{scenario}->{map},
+										debug => $param{debug},
+										
+										);
+	$param{scenario}->{map} = undef;
+	return bless {
+				is_running => 1,
+				current_scenario => '',
+				hero	=> undef,
+				actors	=> [],
+				configuration => $param{configuration} ,
+				ui	=> $param{ui},
+	}, $class;
+}
+
+sub play{
+	my $game = shift;
+	
+		$game->{ui}->draw_map();
+		$game->{ui}->draw_menu( ["hero HP: 42","walk with WASD"] );	
+
+	
+	while($game->{is_running}){
+		# update energy for [hero, actors]
+		# if hero's energy is enough
+		$game->{ui}->show();
+	}
+}
+
+
+
+
+
+
