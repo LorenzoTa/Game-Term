f2f843f97b27b927d95e2b3d763282ed3d8831c3
LorenzoTa <LorenzoTa@users.noreply.github.com>
2019-04-09 20:47:39 +0200

    stupid me I've forgot to add files

---
+++ lib/Game/Term/Actor.pm
@@ -0,0 +1,40 @@
+package Game::Term::Actor;
+
+use 5.014;
+use strict;
+use warnings;
+
+sub new{
+	my $class = shift;
+	my %params = validate_conf(@_);
+	return bless {
+				%params
+	}, $class;
+	
+}
+
+sub validate_conf{
+	my %params = @_;
+	$params{name} //= 'unnamed';
+	$params{hitpoints} //= 1;
+	$params{energy} //= 0;
+	$params{energy_gain} //= 5;
+	$params{energy_gain_multipliers} = {
+		h => 0.8, # hill 0.8
+		m => 0.3, # mountain 3
+		s => 0.1, # swamp 1 
+		t => 0.5, # wood 0.5
+		w => 0.2, # shallow water 2
+		y => 0.5, # wood 0.5 
+	};
+	$params{icon} = 'X';
+	$params{color} = 'Blue';
+	$params{on_tile} = '';
+	$params{x} = undef;
+	$params{y} = undef;
+	return %params;
+}
+
+
+1;
+__DATA__
