41ba302972a826db369bbc35750eab52fe440280
LorenzoTa <LorenzoTa@users.noreply.github.com>
2019-05-09 20:50:32 +0200

    more on get_game_state

--- lib/Game/Term/Game.pm
+++ lib/Game/Term/Game.pm
@@ -77,15 +77,27 @@ sub get_game_state{
 	my $game_state;
 	# check for its content
 	if ( -e -r -s -f $state_file ){
-		$game_state = retrieve( $state_file );
-		#LOAD hero
+		$game_state = retrieve( $state_file ) 
+			or die "Unable to retrieve game state from $state_file";
+		# LOAD hero
 		$game->{hero} = $$game_state->{hero};
 		# reset hero's unneeded fields
 		$game->{hero}{y} = undef;
 		$game->{hero}{x} = undef;
 		$game->{hero}{on_tile} = undef;
 		$game->{hero}{energy} = 0;
-		use Data::Dump; dd $game->{hero};
+		print "DEBUG: loaded HERO from $state_file\n" if $debug;
+		use Data::Dump; dd $game->{hero} if $debug;
+		
+		# eventually LOAD data of the current scenario
+		if(	$$game_state->{ $game->{current_scenario} } ){
+			print "DEBUG: loaded data of '$game->{current_scenario}' from $state_file\n" if $debug;
+		}
+		else{
+			print "DEBUG: no data of '$game->{current_scenario}' in $state_file\n" if $debug;
+		}
+		
+		
 	}
 	# GameState.sto does not exists
 	else {
